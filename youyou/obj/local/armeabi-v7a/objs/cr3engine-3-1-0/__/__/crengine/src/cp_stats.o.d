19061365fca4c6b31109d66be116577f4263d4fb
songzhentao <muchenshou@126.com>
2013-11-27 22:31:34 +0800

    rm obj

--- youyou/obj/local/armeabi-v7a/objs/cr3engine-3-1-0/__/__/crengine/src/cp_stats.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/cr3engine-3-1-0/__/__/crengine/src/cp_stats.o: \
- jni/../../crengine/src/cp_stats.cpp \
- jni/../../crengine/src/../include/cp_stats.h
-
-jni/../../crengine/src/../include/cp_stats.h:
--- youyou/obj/local/armeabi-v7a/objs/cr3engine-3-1-0/__/__/thirdparty/libpng/pngvcrd.o.d
+++
@@ -1,2 +0,0 @@
-obj/local/armeabi-v7a/objs/cr3engine-3-1-0/__/__/thirdparty/libpng/pngvcrd.o: \
- jni/../../thirdparty/libpng/pngvcrd.c
