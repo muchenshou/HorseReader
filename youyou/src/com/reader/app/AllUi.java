e3ec1801144bd58e1326d8583ea98571eeeb5915
muchenshou <muchenshou@126.com>
2014-03-28 17:59:57 +0800

    主界面动画

--- youyou/src/com/reader/app/AllUi.java
+++ youyou/src/com/reader/app/AllUi.java
@@ -1,5 +1,12 @@
 package com.reader.app;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -8,62 +15,123 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
-import com.reader.animation.AnimationView;
-import com.reader.animation.IAnimation;
-import com.reader.animation.SimpleAnimationView;
+import com.reader.util.BitmapUtil;
 
-public class AllUi extends FrameLayout implements IAnimation {
+public class AllUi extends FrameLayout {
 	private View _MainUiView;
 	private View _NetUiView;
-	private AnimationView _animationView;
+	private View _box;
 	private FragmentActivity mfa;
 	private MainUi _MainUi;
-	GestureDetector mGesture = null;
 
+	GestureDetector mGesture = null;
+	Bitmap _bitmap_main;
+	Bitmap _bitmap_net;
+	boolean _animating = false;
+
+	class Box extends View {
+
+		public Box(Context context) {
+			super(context);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			Log.i("song", "box:draw " + _animating);
+			if (_animating) {
+				calcPos();
+				drawAnim(canvas);
+				calcAnimTime();
+
+				postInvalidate();
+			}
+			super.onDraw(canvas);
+		}
+
+	}
+	
 	public AllUi(FragmentActivity fa) {
 		super(fa);
 		mfa = fa;
 		_MainUi = new MainUi(mfa);
 		_MainUiView = _MainUi.create(R.layout.mainui);
 		_NetUiView = new NetUi(mfa).create();
-		_NetUiView.setVisibility(View.GONE);
-
+		_box = new Box(mfa);
+		addView(_box);
 		addView(_NetUiView);
 		addView(_MainUiView);
-		_animationView = new SimpleAnimationView(mfa, this);
 		mGesture = new GestureDetector(mfa, new GestureListener());
 	}
 
 	@Override
-	public int startAnimation(DIR flags) {
-		return 0;
-	}
-
-	@Override
-	public int endAnimation(DIR flags) {
-		return 0;
-	}
-
-	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		if (_animating) {
+			mGesture.onTouchEvent(ev);
+			return true;
+		}
 		return mGesture.onTouchEvent(ev);
 	}
 
+	long _startanim;
+
+	void calcAnimTime() {
+		if (System.currentTimeMillis() > _startanim + 3000) {
+			_animating = false;
+			_MainUiView.setVisibility(View.VISIBLE);
+			_NetUiView.setVisibility(View.VISIBLE);
+
+			postInvalidate();
+
+		}
+	}
+
+	Paint _paint = new Paint();
+	int rotateX = 0;
+	float rotateY = 0;
+	int rotateZ = 0;
+	float translateX = 0;
+	float skewX = 0.9f, skewY=0.9f;
+	
+	void calcPos() {
+		float b = (float) (System.currentTimeMillis() - _startanim) / 3000.0f;
+		translateX =  (b*this.getWidth());
+		rotateY = - (b * pai);
+	}
+	final float pai = 90.0f;
+	int centerX = 0;
+	int centerY = 0;
+	void drawAnim(Canvas canvas) {
+		centerX = getWidth()/2;
+		centerY = getHeight()/2;
+		
+		Camera camera = new Camera();
+		camera.save();
+		camera.rotateY(rotateY);
+		camera.translate(0, 0, 80);
+		Matrix matrix = new Matrix();
+		camera.getMatrix(matrix);
+		camera.restore();
+		//设置翻转中心点   
+        matrix.preTranslate(-this.centerX, -this.centerY);  
+        matrix.postTranslate(this.centerX, this.centerY);  
+        matrix.postTranslate(-translateX, 0);  
+        
+		Bitmap tmpBit = _bitmap_main;
+
+		canvas.drawBitmap(tmpBit, matrix, _paint);
+	}
 
 	class GestureListener extends SimpleOnGestureListener {
 
 		@Override
 		public boolean onDoubleTap(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Log.i("TEST", "onDoubleTap");
-			return super.onDoubleTap(e);
+			return false;
 		}
 
 		@Override
 		public boolean onDown(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Log.i("TEST", "onDown");
-			return super.onDown(e);
+			return false;
 		}
 
 		@Override
@@ -72,6 +140,13 @@ public class AllUi extends FrameLayout implements IAnimation {
 			// TODO Auto-generated method stub
 			Log.i("TEST", "onFling:velocityX = " + velocityX + " velocityY"
 					+ velocityY);
+			_bitmap_main = BitmapUtil.convertViewToBitmap(_MainUiView);
+			_bitmap_net = BitmapUtil.convertViewToBitmap(_NetUiView);
+			_animating = true;
+			_startanim = System.currentTimeMillis();
+			_MainUiView.setVisibility(View.INVISIBLE);
+			_NetUiView.setVisibility(View.INVISIBLE);
+			_box.invalidate();
 			return super.onFling(e1, e2, velocityX, velocityY);
 		}
 
@@ -86,16 +161,17 @@ public class AllUi extends FrameLayout implements IAnimation {
 		public boolean onScroll(MotionEvent e1, MotionEvent e2,
 				float distanceX, float distanceY) {
 			// TODO Auto-generated method stub
-			Log.i("TEST", "onScroll:distanceX = " + distanceX + " distanceY = "
-					+ distanceY);
+			// Log.i("TEST", "action1:"+e1.getAction());
+			// Log.i("TEST", "action2:"+e2.getAction());
+			// Log.i("TEST", "onScroll:distanceX = " + distanceX +
+			// " distanceY = "
+			// + distanceY);
 			return super.onScroll(e1, e2, distanceX, distanceY);
 		}
 
 		@Override
 		public boolean onSingleTapUp(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Log.i("TEST", "onSingleTapUp");
-			return super.onSingleTapUp(e);
+			return false;
 		}
 
 	}
--- youyou/src/com/reader/util/BitmapUtil.java
+++ youyou/src/com/reader/util/BitmapUtil.java
@@ -1,7 +1,24 @@
 package com.reader.util;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.View;
 
 public class BitmapUtil {
-	public static native int DrawBatteryBitmap(Bitmap bitmap, int which ,int color);
+	public static native int DrawBatteryBitmap(Bitmap bitmap, int which,
+			int color);
+
+	public static Bitmap convertViewToBitmap(View view) {
+		Bitmap b = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+				Bitmap.Config.ARGB_8888);
+		Canvas c = new Canvas(b);
+		c.translate(-view.getScrollX(), -view.getScrollY());
+		view.draw(c);
+		view.setDrawingCacheEnabled(true);
+		Bitmap cacheBmp = view.getDrawingCache();
+		Bitmap viewBmp = cacheBmp.copy(Bitmap.Config.ARGB_8888, true);
+		view.destroyDrawingCache();
+
+		return viewBmp;
+	}
 }
